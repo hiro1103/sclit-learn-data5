fa5be7f5ac6cbe811fd97dff11f048d5a2ac6a11
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-13 19:25:17 +0900

    murakami

--- data5.py
+++ data5.py
@@ -109,3 +109,84 @@ plt.ylabel('PC2')
 plt.legend(loc='lower left')
 plt.tight_layout()
 plt.show()
+
+# 決定境界をプロット
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+# 分散説明率を計算
+pca.explained_variance_ratio_
+
+np.set_printoptions(precision=4)
+mean_vecs = []
+for label in range(1, 4):
+    mean_vecs.append(np.mean(X_train_std[y_train == label], axis=0))
+    print('MV %s: %s\n' % (label, mean_vecs[label-1]))
+
+# 特徴量の個数
+d = 13
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.zeros((d, d))
+    for row in X_train_std[y_train == label]:
+        row, mv = row.reshape(d, 1), mv.reshape(d, 1)
+        class_scatter += (row-mv).dot((row-mv).T)
+    S_W += class_scatter
+
+print('Within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+print('Class label distribution: %s' % np.bincount(y_train)[1:])
+
+# 特徴量の個数
+d = 13
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+    class_scatter = np.cov(X_train_std[y_train == label].T)
+    S_W += class_scatter
+print('Scaled within-class scatter matrix: %sx%s' %
+      (S_W.shape[0], S_W.shape[1]))
+
+mean_all = np.mean(X_train_std, axis=0)
+# 特徴量の個数
+d = 13
+S_B = np.zeros((d, d))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train_std[y_train == i+1, :].shape[0]
+    # 列ベクトルを作成
+    mean_vec = mean_vec.reshape(d, 1)
+    mean_all = mean_all.reshape(d, 1)
+    S_B += n*(mean_vec-mean_all).dot((mean_vec-mean_all).T)
+
+print('Between-class scatter matrix: %sx%s' % (S_B.shape[0], S_B.shape[1]))
+
+# inv関数で逆行列、dot関数で行列積、eig関数で固有値を計算
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+               for i in range(len(eigen_vals))]
+
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+print('Eigenvalues in descending order:\n')
+
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+
+# 固有値の実数部の総和を求める
+tot = sum(eigen_vals.real)
+# 分散説明率とその累積和を計算
+discr = [(i/tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+plt.bar(range(1, 14), discr, alpha=0.5, align='center',
+        label='Individual "discriminability"')
+plt.step(range(1, 14), cum_discr, where='mid',
+         label='Cumulative "discriminability"')
+plt.ylabel('"Discriminability" ratio')
+plt.xlabel('Linear Discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
