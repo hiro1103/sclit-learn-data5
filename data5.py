8086670bb578f0a93c0c836fc4ba1c7dfd2b8119
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-13 16:06:19 +0900

    更新

--- data5.py
+++ data5.py
@@ -91,3 +91,21 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
                     edgecolor='black',
                     marker=markers[idx],
                     label=cl)
+
+
+# 主成分数を指定して、PCAのインスタンスを生成
+pca = PCA(n_components=2)
+# ロジスティク回帰のインスタンスを生成
+lr = LogisticRegression(multi_class='ovr', random_state=1, solver='lbfgs')
+# 次元削減
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+# 削減したデータセットでロジスティク回帰モデルを適合
+lr.fit(X_train_pca, y_train)
+# 決定境界をプロット
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
