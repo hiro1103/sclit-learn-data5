d6a5b9fd65dd2d8103ae99f37a1427bddae9ad21
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-14 09:54:11 +0900

    Add files via upload

--- data5.py
+++ data5.py
@@ -1,3 +1,4 @@
+from turtle import color
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
@@ -10,6 +11,7 @@ from scipy.linalg import eigh
 from sklearn.datasets import make_moons
 from matplotlib.ticker import Formatter
 from sklearn.datasets import make_circles
+from sklearn.decomposition import KernelPCA
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -338,5 +340,117 @@ plt.show()
 X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
 plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
 plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+plt.tight_layout()
+plt.show()
+
+# データをPCAで変換してからプロット
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1],
+              color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((500, 1))+0.02,
+              color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((500, 1))-0.02,
+              color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+# データをRBFカーネルPCAで変換してからプロット
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y == 1, 0], X_kpca[y == 1, 1],
+              color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_kpca[y == 0, 0], np.zeros((500, 1))+0.02,
+              color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y == 1, 0], np.zeros((500, 1))-0.02,
+              color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+
+def rbf_kernel_pca1(X, gamma, n_components):
+
+    # M×N次元のデータセットでペアごとのユークリッド距離の2乗を計算
+    sq_dists = pdist(X, 'sqeuclidean')
+
+    # ペアごとの距離を正方行列に変換
+    mat_sq_dists = squareform(sq_dists)
+
+    # 対称カーネル行列を計算
+    K = exp(-gamma*mat_sq_dists)
+
+    # カーネル行列を中心化
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K-one_n.dot(K)-K.dot(one_n)+one_n.dot(K).dot(one_n)
+
+    # 中心化されたカーネル行列から固有対を取得:scipy.linalg.eighはそれらを昇順で返す
+    eigvals, eigvecs = eigh(K)
+    eigvals, eigvecs = eigvals[::-1], eigvecs[:, ::-1]
+
+    # 上位k個の固有ベクトル（射影されたデータ点）を収集
+    alphas = np.column_stack([eigvecs[:, i] for i in range(n_components)])
+
+    # 対応する固有値を収集
+    lambdas = [eigvals[i] for i in range(n_components)]
+    return alphas, lambdas
+
+
+X, y = make_moons(n_samples=100, random_state=123)
+alphas, lambdas = rbf_kernel_pca1(X, gamma=15, n_components=1)
+
+x_new = X[25]
+print(x_new)
+
+# 元の射影
+x_proj = alphas[25]
+print(x_proj)
+
+
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new-row)**2) for row in X])
+    k = np.exp(-gamma*pair_dist)
+    return k.dot(alphas / lambdas)
+
+
+x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+print(x_reproj)
+
+plt.scatter(alphas[y == 0, 0], np.zeros((50)),
+            color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y == 1, 0], np.zeros((50)),
+            color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black', label='Original projection of point X[25]',
+            marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green', label='Remapped point X[25]',
+            marker='x', s=500)
+plt.yticks([], [])
+plt.legend(scatterpoints=1)
+plt.tight_layout()
+plt.show()
+
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y == 0, 0], X_skernpca[y == 0, 1],
+            color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y == 1, 0], X_skernpca[y == 1, 1],
+            color='blue', marker='o', alpha=0.5)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
 plt.tight_layout()
 plt.show()
